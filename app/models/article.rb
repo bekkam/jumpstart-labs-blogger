fa818bd1aef52608e4fbfb2324f09620ebc648b6
Bekka Murphy <bekkam@users.noreply.github.com>
2016-08-31 22:20:13 -0700

    Add tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+
+    # method to display related tags
+    def tag_list
+        tags.join(", ")
+    end
+
+    def tag_list=(tags_string)
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+        # rails built in method find_or_create_by searches for a tag_name 'name', and if it doesnt exist, creates one
+        new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+        self.tags = new_or_found_tags
+    end
 end
